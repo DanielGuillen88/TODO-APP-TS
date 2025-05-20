d0e3c50e258f03abb9139ade2870d73aa796582d
DanielGuillen88 <danielguillen85@gmail.com>
2025-05-20 18:15:18 +0200

    creamos los todos componentes / create toodos components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-const App = () => {
+import { useState } from "react"
+import { Todos } from "./assets/components/Todos"
+
+const mockTodos = [
+  { id: '1', 
+    title: 'todo 1', 
+    completed: false 
+  },
+  { id: '2', 
+    title: 'todo 2', 
+    completed: false 
+  },
+  { id: '3', 
+    title: 'todo 3', 
+    completed: false 
+  }
+]
+const App: React.FC = () => {
+
+  const [ todos ] = useState(mockTodos)
+
   return (
-    <>
+    <div className="todoapp">
 
       <h1>TODO MVC</h1>
+      <Todos todos = { todos } />
       
-    </>
+    </div>
   )
 }
 
---
+++ src/assets/components/Todo.tsx
@@ -0,0 +1,21 @@
+import { type Todo as TodoType } from "./types"
+
+type TodoProps = TodoType 
+
+export const Todo: React.FC<TodoProps> = ({ id, title, completed }) => {
+    return (
+        <div className="view">
+            <input
+                className="toggle"
+                type="checkbox"
+                checked={completed}
+                onChange={() => {}}
+            />
+            <label>{title}</label>
+            <button
+                className="destroy"
+                onClick={() => {}}
+            />
+        </div>
+    )
+}
