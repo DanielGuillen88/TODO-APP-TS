2b95a0ebd4189731a45cbb7ac19d5ca0ce7e6799
DanielGuillen88 <danielguillen85@gmail.com>
2025-05-20 18:07:49 +0200

    quitar estilos css por defecto/remove default css styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 import 'todomvc-app-css/index.css'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
